e57d3dae2d846ff6a6325d9687691a17d36d82ad
Zhaoze Wang <wangzhaoze@outlook.com>
2025-05-31 19:02:43 +0200

    init commit

--- pyradar/cfg/sampler.py
+++ pyradar/cfg/sampler.py
@@ -20,12 +20,6 @@ class Sampler:
     numSamplesPerChirps: Optional[int] = field(
         default=None
     )  # Number of samples per chirp
-    frameDuration: Optional[float] = field(
-        default=None
-    )  # Duration of one frame (in seconds)
-    chirpDuration: Optional[float] = field(
-        default=None
-    )  # Duration of each chirp (in seconds)
     adcSampleRate: Optional[float] = field(default=None)  # ADC sample rate
 
     def __post_init__(self):
@@ -33,9 +27,7 @@ class Sampler:
             self.numChirpsperFrame = self.frameDuration / self.chirpDuration
         if self.numSamplesPerChirps is None:
             self.numSamplesPerChirps = self.adcSampleRate * self.chirpDuration
-        if self.frameDuration is None:
-            self.frameDuration = self.numChirpsperFrame * self.chirpDuration
-        if self.chirpDuration is None:
-            self.chirpDuration = self.frameDuration / self.numChirpsperFrame
+
         if self.adcSampleRate is None:
             self.adcSampleRate = self.numSamplesPerChirps / self.chirpDuration
+            
